6a02bb8e3742014896e6b315e71360985012ea6a
kanvhcq <60723603+kanvhcq@users.noreply.github.com>
2020-02-10 15:57:15 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,13 +3,15 @@
 $url = parse_url(getenv("DATABASE_URL"));
 
 return (object) array(
-    'db_server' =>$url["host"],
-    'db_username' => $url["user"],
-    'db_pass' => $url["pass"],
-    'db_database' => substr($url["path"], 1),
-    'channelID' => '*******',
-    'channelAccessToken' => '*******',
-    'channelSecret' => '*******',
+    'db_server' =>$url["localhost"],
+    'db_username' => $url["ppdata_kan"],
+    'db_pass' => $url["Ptc#02290"],
+    'db_database' => substr($url["ppdata_kan"], 1),
+    'channelID' => 'Uee4a391925a5eef42e4a0d6e9c56c5c4',
+    'channelAccessToken' => 'uH7/FdqH5XQrxB0t4yGuZmgfl92Y7ZpFa45bsGLitYHkE+eqbB+Sind850wztPqItL4WvDJjLW2eozCwS9Scs0dllMVR/wjLDG55+TzIk9/gPH10nYKQogSqbja+x+ZSAx1uR3aFpOfeNhjNplZhhAdB04t89/1O/w1cDnyilFU=',
+    'channelSecret' => '98cb4e8e9de7032d538b36851e9c9d48
+
+',
 
 );
 
